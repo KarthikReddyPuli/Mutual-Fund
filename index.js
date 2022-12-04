da7e3294f04fe62aa3794c99c06bfb588c04208c
KarthikReddyPuli <karthik.puli@zomato.com>
2022-12-04 17:19:20 +0530

    Linters

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const xlsx = require('xlsx');
 const getFundDetails = require('./getFundDetails');
 
+const investmentAmount = 18000
+
 const main = async () => {
     getFundDetails("INF966L01614").then((resp) => {
         const workbook = xlsx.utils.book_new();
         const worksheet = xlsx.utils.json_to_sheet(resp.data.map(item => {
+            percentValue = parseFloat(item[5])
             return {
                 "Company Name": item[1],
-                "Percentage": item[5],
+                "Percentage": percentValue,
+                "Amount": (investmentAmount * percentValue) / 100
             }
         }));
         xlsx.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-        xlsx.writeFile(workbook, "test.xlsx");
+        xlsx.writeFile(workbook, "excel/test.xlsx");
     }).catch((error) => {
         console.log(error);
     })
